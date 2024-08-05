1943062fef5c8775a1b85322ad7922806f07ed99
VenkataRamanaRao5 <venkatraocv2005@gmail.com>
2024-08-05 22:28:09 +0530

    Create editor

---
+++ src/Editor.tsx
@@ -0,0 +1,21 @@
+import { useState } from "react";
+
+interface Props {
+    tableElems: string[][];
+    setTableElems: (e: string) => void;
+}
+
+export default function Editor({tableElems, setTableElems}: Props){
+    const [tableInput, setTableInput] = useState(tableElems.map(e => e.reduce((l, r) => `${l};${r}`)).reduce((l, r) => `${l}\n${r}`))
+    return (
+        <>
+            <textarea 
+                className="output" 
+                onChange={(e) => setTableInput(e.currentTarget.value)}
+                value={tableInput}
+            />
+            <button type="button" onClick={() => setTableElems(tableInput)}>Set</button>
+            <button type="button" onClick={() => setTableElems('')}>Clear</button>
+        </>
+    )
+} 
